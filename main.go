57fac6e5650eb694d1a095539f897e77120c7040
太田勝博 <styxcab@gmail.com>
2021-03-04 23:13:28 +0900

    output only pdf

--- main.go
+++ main.go
@@ -3,26 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
+	"strings"
 )
 
 func main() {
-	// 今のディレクトリの場所を取ってくるやつ
-	dir, err := os.Getwd()
+	// // 今のディレクトリの場所を取ってくるやつ
+	// dir, err := os.Getwd()
 
-	//　表示する
-	fmt.Println(dir)
-	fmt.Println(err)
+	// //　表示する
+	// fmt.Println(dir)
+	// fmt.Println(err)
 
 	// ファイルの中身
-	fileInfos, err := ioutil.ReadDir(dir)
+	fileInfos, err := ioutil.ReadDir("./test_pdf")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("******************************")
+	// fmt.Println(fileInfos)
 
 	// PDFがあるディレクトリで↑をして絞り込みたい
 	for _, fileInfo := range fileInfos {
-		fmt.Println(fileInfo.Name())
+		// pdfのみ出力する
+		if strings.Contains(fileInfo.Name(), ".pdf") {
+			// 型番で検索
+			if strings.Contains(fileInfo.Name(), "") {
+				fmt.Println(fileInfo.Name())
+			}
+		}
 	}
 }
