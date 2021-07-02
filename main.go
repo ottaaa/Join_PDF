a986ccdce085965d2383a4683c2f9a50ce2dd2de
太田勝博 <styxcab@gmail.com>
2021-07-02 16:06:42 +0900

    add fyne demo page

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	//"bufio"
+	//"fmt"
 	"index/suffixarray"
 	"os"
 	"path/filepath"
@@ -10,24 +10,46 @@ import (
 
 	"github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	// 型番を聞く
-	fmt.Print("型番を半角スペース区切りで入力してください >")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	input := scanner.Text()
-	fmt.Println("in: ", input)
-	input_arr := strings.Split(input, " ")
-	fmt.Println("in: ", input_arr)
-	// AAB000001 AAB000002
-	// 結合する型番
-	var modelNum = input_arr
-	// 結合するPDFファイルのパスを取得
-	var targetPDF = getPDFPath(modelNum)
-	// 結合
-	merge(targetPDF)
+
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+
+	//// TODO ビルド
+	//// GOOS=windows GOARCH=amd64 go build main.go
+	//// ビルドした後にwinでの動作検証
+	//
+	//// 型番を聞く
+	//fmt.Print("型番を半角スペース区切りで入力してください >")
+	//scanner := bufio.NewScanner(os.Stdin)
+	//scanner.Scan()
+	//input := scanner.Text()
+	//fmt.Println("in: ", input)
+	//input_arr := strings.Split(input, " ")
+	//fmt.Println("in: ", input_arr)
+	//// AAB000001 AAB000002
+	//// 結合する型番
+	//var modelNum = input_arr
+	//// 結合するPDFファイルのパスを取得
+	//var targetPDF = getPDFPath(modelNum)
+	//// 結合
+	//merge(targetPDF)
 }
 
 func contains(src string, words []string) bool {
